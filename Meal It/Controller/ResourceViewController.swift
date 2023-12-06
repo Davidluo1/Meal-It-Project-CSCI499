37dc71a7aed1f6f521c73b063a538ded8b19a64c
David Luo <david.luo08@myhunter.cuny.edu>
2023-12-06 13:03:03 -0500

    Update ResourceViewController.swift

--- Meal It/Controller/ResourceViewController.swift
+++ Meal It/Controller/ResourceViewController.swift
@@ -1,23 +1,18 @@
-//
 //  ResourceViewController.swift
 //  Meal It
-//
-//
+
 
 import UIKit
 
 class ResourceViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
     @IBAction func backButtonAction(){
 
         self.navigationController?.popViewController(animated: true)
     }
-
 }
