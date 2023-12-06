6c5228af082613b0cebc04b0548aebb4ded85521
David Luo <david.luo08@myhunter.cuny.edu>
2023-12-06 13:00:01 -0500

    Update AboutViewController.swift

--- Meal It/Controller/AboutViewController.swift
+++ Meal It/Controller/AboutViewController.swift
@@ -1,24 +1,19 @@
-//
 //  AboutViewController.swift
 //  Meal It
-//
-//
+
 
 import UIKit
 
 class AboutViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
 
     @IBAction func backButtonAction(){
 
         self.navigationController?.popViewController(animated: true)
 
     }
-
 }
