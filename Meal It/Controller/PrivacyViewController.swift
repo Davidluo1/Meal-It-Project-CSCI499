2f99727c3431913b2b11396f1931e915beac8614
David Luo <david.luo08@myhunter.cuny.edu>
2023-12-06 13:02:26 -0500

    Update PrivacyViewController.swift

--- Meal It/Controller/PrivacyViewController.swift
+++ Meal It/Controller/PrivacyViewController.swift
@@ -1,23 +1,18 @@
-//
 //  PrivacyViewController.swift
 //  Meal It
-//
-//
+
 
 import UIKit
 
 class PrivacyViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
     @IBAction func backButtonAction(){
 
         self.navigationController?.popViewController(animated: true)
     }
-
 }
