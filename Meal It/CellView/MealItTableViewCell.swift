5c0a04e928c80169863ed0b416a8d608dd4ed0d1
David Luo <david.luo08@myhunter.cuny.edu>
2023-12-07 09:56:24 -0500

    Update MealItTableViewCell.swift

--- Meal It/CellView/MealItTableViewCell.swift
+++ Meal It/CellView/MealItTableViewCell.swift
@@ -1,17 +1,10 @@
-//
 //  MealTableViewCell.swift
 //  Meal It
-//
-//
 
 import UIKit
 
 class MealItTableViewCell: UITableViewCell {
-
     @IBOutlet weak var Title: UILabel!
     @IBOutlet weak var smoothieImage: Custom_ImageView!
     
-    
-  
-
 }
