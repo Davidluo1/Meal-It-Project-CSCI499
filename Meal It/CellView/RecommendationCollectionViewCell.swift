df56734467a782bbef70cc19b854b26f321ecc76
David Luo <david.luo08@myhunter.cuny.edu>
2023-12-07 09:56:37 -0500

    Update RecommendationCollectionViewCell.swift

--- Meal It/CellView/RecommendationCollectionViewCell.swift
+++ Meal It/CellView/RecommendationCollectionViewCell.swift
@@ -1,13 +1,10 @@
-//
 //  RecommendationCollectionViewCell.swift
 //  Meal It
-//
-//
+
 
 import UIKit
 
 class RecommendationCollectionViewCell: UICollectionViewCell {
-    
     @IBOutlet weak var Title: UILabel!
     @IBOutlet weak var smoothieImage: UIImageView!
 }
