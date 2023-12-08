31bbd37c94866ddc35f2e3b5f0116b46a675d628
David Luo <david.luo08@myhunter.cuny.edu>
2023-12-08 10:03:19 -0500

    Update ConfigureUserInterface.swift

--- Meal It/Customize UI/ConfigureUserInterface.swift
+++ Meal It/Customize UI/ConfigureUserInterface.swift
@@ -1,19 +1,14 @@
-//
 //  ConfigureUserInterface.swift
-//
-//
+
 
 import Foundation
 import UIKit
 
 public class ConfigureUserInterface: NSObject{
-    
-    
     public static func configureNavigationBar(navBar : UINavigationBar){
         navBar.setBackgroundImage(UIImage(), for: .default)
         navBar.shadowImage = UIImage()
         navBar.isTranslucent = true
         navBar.backgroundColor = UIColor.clear
     }
-    
 }
